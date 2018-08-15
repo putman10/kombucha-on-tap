6938ce69dba23843a03e3e8858e26d168c144b1b
Rita Bennett-Chew and Ryan Putman <ryan@>
2018-08-15 10:26:21 -0700

    add keg constructor and UI loop to display kegs in a table.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "Kombucha Tap"
+  kegs: Keg[] = [
+    new Keg (
+      'Blue Steel',
+      'Zoolander',
+      5, 0.05, ["stiller", "movie-inspired"], 12
+    ),
+    new Keg (
+      'Blue Sky', 'Store', 6, 0.00003, ['oil', 'vinegar'], 8
+    ),
+    new Keg ('Crocodile Dung', 'Egyptian Contraceptive', 4, 0.1, ['dont consume', 'ancient'], 60)
+  ]
 }
---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,3 @@
+export class Keg {
+  constructor(public name: string, public brand: string, public pricePerPint: number, public alcoholContent: number, public flavors: string[], public pintsRemaining: number) {}
+}
